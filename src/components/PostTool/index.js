3854bac7037e652472f50ae30cd8948d5763996e
Pham Thang <bachangngheo9a@gmail.com>
2020-11-06 23:14:43 +0700

    Continue design FullPostTool screen

--- src/components/PostTool/index.js
+++ src/components/PostTool/index.js
@@ -1,56 +1,59 @@
-import React from 'react'
-import { Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { Text, StyleSheet, View, Image, Pressable } from 'react-native'
 import { connect } from 'react-redux';
 import * as Colors from '../../assets/Colors';
 import {navigation} from '../../../rootNavigation';
-export default function PostTool() {
-    
+
+export default function PostTool() {    
+    const [press, setPress] = useState(false);
     const onFullPostToolPressHandler = () => {
         navigation.navigate('FullPostTool')
     }
+    const styles = StyleSheet.create({
+        container: {
+            borderTopColor: Colors.LIGHTGRAY,
+            borderTopWidth: 0.5,
+            borderBottomColor: Colors.LIGHTGRAY,
+            borderBottomWidth: 0.5,
+            backgroundColor: Colors.WHITE,
+            padding: 10,
+            flexDirection: 'row'
+        },
+        postInputWrapper: {
+            borderRadius: 48,
+            flex: 1,
+            marginLeft: 10,
+            justifyContent: 'center',
+            borderRadius: 20,
+            height: 40,
+            width: "100%",
+            borderColor: Colors.LIGHTGRAY,
+            paddingHorizontal: 20,
+            borderWidth: 1,
+            backgroundColor: press ? Colors.GAINSBORO : Colors.WHITE,
+        },
+        userAvatar: {
+            width: 40,
+            height: 40,
+            borderRadius: 50,
+        },
+    })
+    
     return (
         <View style={styles.container}>
-            <TouchableOpacity activeOpacity={0.5}>
+            <Pressable>
                 <Image source={{ uri: "https://scontent.fhan9-1.fna.fbcdn.net/v/t1.0-9/109937103_1147876648925466_5407335593471103700_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=uKzWS_yxgZAAX9-JzFG&_nc_ht=scontent.fhan9-1.fna&oh=91377309008559c505359fd91e291b66&oe=5FCB1B0D" }} style={styles.userAvatar} ></Image>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => onFullPostToolPressHandler()} style={styles.postInputWrapper}>
-                <View style={styles.postInput}>
-                    <Text style={{fontSize: 16}}>Bạn đang nghĩ gì ?</Text>
-                </View>
-            </TouchableOpacity>
+            </Pressable>
+            <Pressable 
+                onPress={() => onFullPostToolPressHandler()} 
+                style={styles.postInputWrapper}
+                onPressIn={() => setPress(true)}
+                onPressOut={() => setPress(false)}
+            >
+                <Text style={{fontSize: 16}}>Bạn đang nghĩ gì?</Text>
+            </Pressable>
         </View>
     )
     
 }
 
-const styles = StyleSheet.create({
-    container: {
-        borderTopColor: Colors.LIGHTGRAY,
-        borderTopWidth: 0.5,
-        borderBottomColor: Colors.LIGHTGRAY,
-        borderBottomWidth: 0.5,
-        backgroundColor: Colors.WHITE,
-        padding: 10,
-        flexDirection: 'row'
-    },
-    postInputWrapper: {
-        borderRadius: 48,
-        flex: 1,
-        marginLeft: 5,
-    },
-    postInput: {
-        justifyContent: 'center',
-        borderRadius: 48,
-        height: 40,
-        width: "100%",
-        borderColor: Colors.GAINSBORO,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        backgroundColor: Colors.WHITE,
-    },
-    userAvatar: {
-        width: 40,
-        height: 40,
-        borderRadius: 50,
-    },
-})
--- src/screens/App/HomeTab/FullPostTool.js
+++ src/screens/App/HomeTab/FullPostTool.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import {View, StyleSheet, Text, Image, TextInput} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text, Image, TextInput, Pressable, Keyboard} from 'react-native';
 import Ant from 'react-native-vector-icons/AntDesign';
 import Oct from 'react-native-vector-icons/Octicons';
-import Font5 from 'react-native-vector-icons/FontAwesome5';
-
+import Fontisto from 'react-native-vector-icons/Fontisto';
 import * as Colors from '../../../assets/Colors'
-export default function FullPostTool() {
+
+export default function FullPostTool({navigation}) {
+    const [press, setPress] = useState(0);
+    const [show, setShow] = useState(true);
+    const [content, setContent] = useState("");
     const styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -22,14 +25,25 @@ export default function FullPostTool() {
         },
         title: {
             fontSize: 17,
+            marginLeft: 5,
         },
-        post: {
+        postContainer: {
             position: "absolute",
-            right: 20,
+            right: 5,
+            backgroundColor: press == 4 ? Colors.LIGHTGRAY : Colors.WHITE,
+            height: 46,
+            width: 60,
+            borderRadius: 5,
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        postText: {
+            color: content.length > 0 ? Colors.BLUE : Colors.GAINSBORO
         },
         headContainer: {
             flexDirection: "row",
-            margin: 15
+            marginHorizontal: 15,
+            marginTop: 15,
         },
         avt: {
             width: 50,
@@ -45,7 +59,7 @@ export default function FullPostTool() {
         },
         public: {
             flexDirection: 'row',
-            width: 110,
+            width: 100,
             height: 30,
             borderRadius: 5,
             borderColor: Colors.LIGHTGRAY,
@@ -57,7 +71,7 @@ export default function FullPostTool() {
         publicText: {
             fontWeight: 'bold',
             color: Colors.GRAY,
-            fontSize: 13,
+            fontSize: 12,
         },
         input: {
             fontSize: 25,
@@ -78,14 +92,68 @@ export default function FullPostTool() {
             flexDirection: 'row',
             position: 'absolute',
             right: 10,
+        },
+        backIcon: {
+            backgroundColor: press == 1 ? Colors.LIGHTGRAY : Colors.WHITE,
+            width: 40,
+            height: 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 20,
+        },
+        line1: {
+            flexDirection: 'row',
+            width: '100%',
+            height: 50,
+            borderTopColor: Colors.LIGHTGRAY,
+            borderTopWidth: 0.5,
+            alignItems: 'center',
+            padding: 15,
+            backgroundColor: press == 2 ? Colors.GAINSBORO : Colors.WHITE,
+        },
+        line2: {
+            flexDirection: 'row',
+            width: '100%',
+            height: 50,
+            borderTopColor: Colors.LIGHTGRAY,
+            borderTopWidth: 0.5,
+            alignItems: 'center',
+            padding: 15,
+            backgroundColor: press == 3 ? Colors.GAINSBORO : Colors.WHITE
         }
     })
+
+    useEffect(() => {
+        Keyboard.addListener('keyboardDidShow', _keyboardDidShow);    
+        // cleanup function
+        return () => {
+          Keyboard.removeListener('keyboardDidShow', _keyboardDidShow);
+        };
+      }, []);
+    
+      const _keyboardDidShow = () => {
+        setShow(false);
+      };
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
-                <Ant name="arrowleft" size={24} style={{margin: 10}}/>
+                <Pressable style={styles.backIcon} 
+                    onPress={() => navigation.goBack()}
+                    onTouchStart={() => setPress(1)}
+                    onTouchEnd={() => setPress(0)}
+                >
+                    <Ant name="arrowleft" size={24} />
+                </Pressable>
                 <Text style={styles.title}>Tạo bài viết</Text>
-                <Text style={styles.post}>ĐĂNG</Text>
+                <Pressable 
+                    disabled={content.length == 0}
+                    style={styles.postContainer}
+                    onTouchStart={() => {if(content.length > 0) setPress(4)}}
+                    onTouchEnd={() => setPress(0)}
+                >
+                    <Text style={styles.postText}>ĐĂNG</Text>
+                </Pressable>
             </View>
             <View style={styles.headContainer}>
                 <Image source={{uri: "https://scontent.fhan9-1.fna.fbcdn.net/v/t1.0-9/109937103_1147876648925466_5407335593471103700_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=uKzWS_yxgZAAX9-JzFG&_nc_ht=scontent.fhan9-1.fna&oh=91377309008559c505359fd91e291b66&oe=5FCB1B0D"}} style={styles.avt}/>
@@ -101,16 +169,29 @@ export default function FullPostTool() {
                 placeholder="Bạn đang nghĩ gì?"
                 placeholderTextColor={Colors.GRAY}
                 style={styles.input}
+                onChangeText={(text) => setContent(text.trim())}
+                multiline={true}
             />
-            <View style={styles.addContainer}>
+            {show ? 
+            <View style={{position: "absolute", bottom: 0, width: '100%'}}>
+                <Pressable style={styles.line1} onPressIn={() => setPress(2)} onPressOut={() => setPress(0)}>
+                    <Fontisto style={{marginRight: 15}} name="photograph" color={Colors.LIME} size={24}/>
+                    <Text style={{fontSize: 16}}>Ảnh/Video</Text>
+                </Pressable>
+                <Pressable style={styles.line2} onPressIn={() => setPress(3)} onPressOut={() => setPress(0)}>
+                    <Fontisto style={{marginRight: 15}} name="smiley" size={24} color={Colors.ORANGE}/>
+                    <Text style={{fontSize: 16}}>Cảm xúc/Hoạt động</Text>
+                </Pressable>
+            </View>
+            :
+            <Pressable style={styles.addContainer} onPress={() => {setShow(true); Keyboard.dismiss()}}>
                 <Text style={{fontSize: 16}}>Thêm vào bài viết của bạn</Text>
                 <View style={styles.iconGroup}>
-                    <Oct name="device-camera-video" size={24}/>
-                    <Ant  name="picture" size={24}/>
-                    <Font5 name="user-tag" size={24}/>
-                    <Oct name="smiley" size={24}/>
+                    <Fontisto style={{marginRight: 5}} name="photograph" color={Colors.LIME} size={24}/>
+                    <Fontisto name="smiley" size={24} color={Colors.ORANGE}/>
                 </View>
-            </View>
+            </Pressable> 
+            }
         </View>
     )
 }
