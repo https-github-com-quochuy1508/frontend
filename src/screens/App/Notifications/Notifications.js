390fcd1c40163888802582d54ce4835a02eca605
nguyenxuanhoat9 <57888255+nguyenxuanhoat9@users.noreply.github.com>
2020-12-13 22:13:29 +0700

    search screen

--- src/screens/App/Notifications/Notifications.js
+++ src/screens/App/Notifications/Notifications.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View} from "react-native";
+import { View,  TouchableOpacity,Button,Text,} from "react-native";
+import SearchForm from '../../../components/search/SearchForm';
 
-export default function Notifications() {
+export default function Notifications({navigation}) {
   return (
-    <View/>
+    <View>
+        <TouchableOpacity >
+          <Button title="Press To Search" 
+          onPress={() => navigation.navigate("SearchForm")} activeOpacity={0.8}
+          >
+          </Button>
+          </TouchableOpacity>
+    </View>
   );
 }
--- src/screens/App/Notifications/index.js
+++ src/screens/App/Notifications/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Notifications from './Notifications';
+import SearchForm from '../../../components/search/SearchForm';
+import SearchResult from '../../../components/search/SearchResult';
 const Stack = createStackNavigator();
 
 export default function NotificationsStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="SearchForm" component={SearchForm} />
+      <Stack.Screen name="Nhật ký hoạt động" component={SearchResult} />
     </Stack.Navigator>
   );
 }
