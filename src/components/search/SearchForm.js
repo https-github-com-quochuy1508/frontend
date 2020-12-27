d03b84d9590a42c792d5f2c264d625bcae6b66bc
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-12-27 14:55:43 +0700

    APi for personal

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -14,97 +14,98 @@ import * as Colors from '../../assets/Colors';
 import Icon from 'react-native-vector-icons/Ionicons';
 import SearchHistoryPane from './SearchHistoryPane';
 
-export default function SearchForm({navigation}){
+export default function SearchForm({navigation}) {
   const onPress = () => {};
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.inputSearch}>
-          <TouchableHighlight
-            style={styles.back_button}
-            onPress={() => navigation.goBack()}
-            underlayColor={'#ccc'}>
-            <Icon name="arrow-back" size={25} />
-          </TouchableHighlight>
-          <TextInput
-            style={styles.inputForm}
-            placeholder="Tìm kiếm "
-            autoFocus={true}
-          />
-        </View>
-
-        <ScrollView style={styles.listSearch}>
-          <View style={styles.recent}>
-            <Text style={{fontWeight: "700", fontSize:15,}}>Mới đây </Text>
-            <TouchableHighlight
-              style={styles.editButton}
-              onPress={() => navigation.navigate("SearchDiary")} activeOpacity={0.8}
-              underlayColor={'#ccc'}>
-              <Text style={{color: '#666', fontSize:15}}>CHỈNH SỬA</Text>
-            </TouchableHighlight>
-          </View>
-          <View style={styles.history}>
-            <SearchHistoryPane keyword="ascasd" />
-            <SearchHistoryPane keyword="ascasd" />
-            <SearchHistoryPane keyword="ascasd" />
-          </View>
-        </ScrollView>
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputSearch}>
+        <TouchableHighlight
+          style={styles.back_button}
+          onPress={() => navigation.goBack()}
+          underlayColor={'#ccc'}>
+          <Icon name="arrow-back" size={25} />
+        </TouchableHighlight>
+        <TextInput
+          style={styles.inputForm}
+          placeholder="Tìm kiếm "
+          autoFocus={true}
+        />
       </View>
-    );
-} 
+
+      <ScrollView style={styles.listSearch}>
+        <View style={styles.recent}>
+          <Text style={{fontWeight: '700', fontSize: 15}}>Mới đây </Text>
+          <TouchableHighlight
+            style={styles.editButton}
+            onPress={() => navigation.navigate('SearchDiary')}
+            activeOpacity={0.8}
+            underlayColor={'#ccc'}>
+            <Text style={{color: '#666', fontSize: 15}}>CHỈNH SỬA</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.history}>
+          <SearchHistoryPane keyword="Hồ Quốc Huy" />
+          <SearchHistoryPane keyword="Phạm đình thắng" />
+          <SearchHistoryPane keyword="Nguyễn Xuân Hoạt" />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor:'#FFF',
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
     //   height:100,
-    },
-    listSearch:{
-      flex:1,
-    },
-    inputForm:{
-        height:'78%',
-        width:'88%',
-        borderRadius:30,
-        fontSize:16,
-        backgroundColor: Colors.WHITESMOKE,
-        paddingHorizontal: 20,
-      },
-      inputSearch:{
-        width:'100%',
-        flexDirection:'row',
-        justifyContent:'space-around',
-        height: 52,
-        backgroundColor:Colors.WHITE,
-        alignItems:'center',
-        paddingHorizontal:10,
-        borderBottomColor:'#ccc',
-        borderBottomWidth:0.5, 
-      },
-      back_button:{
-        width: 40,
-        height: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-        borderRadius: 100,
-        // backgroundColor: Colors.GAINSBORO,
-      },
-      recent:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        paddingVertical:8,
-        paddingHorizontal:15,
-        borderBottomWidth:0.5,
-        borderBottomColor:'#ccc',
-      },  
-      editButton:{
-        padding:2,
-        paddingHorizontal:5,
-        borderRadius:3,
-      },
-      history:{
-        // padding:10,
-        // alignItems:'center',
-      },
+  },
+  listSearch: {
+    flex: 1,
+  },
+  inputForm: {
+    height: '78%',
+    width: '88%',
+    borderRadius: 30,
+    fontSize: 16,
+    backgroundColor: Colors.WHITESMOKE,
+    paddingHorizontal: 20,
+  },
+  inputSearch: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 52,
+    backgroundColor: Colors.WHITE,
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 0.5,
+  },
+  back_button: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    // backgroundColor: Colors.GAINSBORO,
+  },
+  recent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#ccc',
+  },
+  editButton: {
+    padding: 2,
+    paddingHorizontal: 5,
+    borderRadius: 3,
+  },
+  history: {
+    // padding:10,
+    // alignItems:'center',
+  },
 });
--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -14,7 +14,6 @@ import Messenger from '../../components/MessengerButton';
 import Setting from './PersonalTab/Setting';
 import Edit from './PersonalTab/Edit';
 import Chat from './Chat/Chat';
-import Friend from './FriendTab';
 import FA from 'react-native-vector-icons/FontAwesome';
 import Mat from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -52,65 +51,101 @@ export default function AppStack({navigation}) {
       <Stack.Screen
         name="App"
         component={AppTabs}
-        options={
-          {
-            headerTitle: "fakebook",
-            headerTitleStyle: {
-              color: Colors.BLUE,
-              fontWeight: "bold",
-              fontSize: 28,
-              fontFamily: "Roboto",
-            },
-            headerStyle: {
-              elevation: 0
-            },
-            headerRight: () =>
-              <View style={{ flexDirection: 'row' }}>
-                <Search />
-                <Messenger />
-              </View>
-          }
-        }
+        options={{
+          headerTitle: 'fakebook',
+          headerTitleStyle: {
+            color: Colors.BLUE,
+            fontWeight: 'bold',
+            fontSize: 28,
+            fontFamily: 'Roboto',
+          },
+          headerStyle: {
+            elevation: 0,
+          },
+          headerRight: () => (
+            <View style={{flexDirection: 'row'}}>
+              <Search />
+              <Messenger />
+            </View>
+          ),
+        }}
       />
-      <Stack.Screen name="Chat" component={Chat} options={{
-        headerTitle: '',
-        headerRight: () => (
-          <View style={{ marginRight: 30, flexDirection: 'row', justifyContent: 'center', }}>
-            <FA name='phone' size={22} color={Colors.BLUE} style={{ marginRight: 30 }}></FA>
-            <FA name='video-camera' size={22} color={Colors.BLUE} style={{ marginRight: 30 }}></FA>
-            <Pressable
-              style={[
-                { backgroundColor: press == 1 ? Colors.GAINSBORO : Colors.WHITE },
-              ]}
-              onTouchStart={() => setPress(1)}
-              onTouchEnd={() => setPress(0)}
-              onPressOut={() => setPress(0)}
-            >
-              <Mat name='information' size={22} color={Colors.BLUE}></Mat>
-            </Pressable>
-          </View>
-        ),
-        headerLeft: () => (
-          <View style={{ marginLeft: 30, flexDirection: 'row', justifyContent: 'center', }}>
-            <Pressable style={{flexDirection: 'row', justifyContent: 'center',}}>
-              <Image style={{width: 35, height: 35, borderRadius: 20}} source={{ uri: "https://placeimg.com/50/50/animals" }}></Image>
-              <Text style={{fontSize: 17, marginLeft: 10, marginTop: 3}}>Quân</Text>
-            </Pressable>
-          </View>
-        ),
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        }
-      }} />
-      <Stack.Screen name="Setting" component={Setting} options={{
-        headerTitle: 'Cài đặt trang cá nhân',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        }
-      }} />
-      <Stack.Screen name="Edit" component={Edit} options={{
-        headerTitle: 'Chỉnh sửa trang cá nhân',
-      }} />
+      <Stack.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          headerTitle: '',
+          headerRight: () => (
+            <View
+              style={{
+                marginRight: 30,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <FA
+                name="phone"
+                size={22}
+                color={Colors.BLUE}
+                style={{marginRight: 30}}></FA>
+              <FA
+                name="video-camera"
+                size={22}
+                color={Colors.BLUE}
+                style={{marginRight: 30}}></FA>
+              <Pressable
+                style={[
+                  {
+                    backgroundColor:
+                      press == 1 ? Colors.GAINSBORO : Colors.WHITE,
+                  },
+                ]}
+                onTouchStart={() => setPress(1)}
+                onTouchEnd={() => setPress(0)}
+                onPressOut={() => setPress(0)}>
+                <Mat name="information" size={22} color={Colors.BLUE}></Mat>
+              </Pressable>
+            </View>
+          ),
+          headerLeft: () => (
+            <View
+              style={{
+                marginLeft: 30,
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <Pressable
+                style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <Image
+                  style={{width: 35, height: 35, borderRadius: 20}}
+                  source={{uri: 'https://placeimg.com/50/50/animals'}}></Image>
+                <Text style={{fontSize: 17, marginLeft: 10, marginTop: 3}}>
+                  Quân
+                </Text>
+              </Pressable>
+            </View>
+          ),
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          headerTitle: 'Cài đặt trang cá nhân',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Edit"
+        component={Edit}
+        options={{
+          headerTitle: 'Chỉnh sửa trang cá nhân',
+        }}
+      />
     </Stack.Navigator>
   );
 }
